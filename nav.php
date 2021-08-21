92a9da752cce39f292672f239ecaf6509f1fbb4a
15aastha <86902909+15aastha@users.noreply.github.com>
2021-08-21 11:47:46 +0530

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,43 @@
+<!-- NAVBAR -->
+
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"></a>
+    <img src="photos/logo3new.jpg" alt="" width="80" height="35">
+                
+ 
+  <!-- till what point do you want the screen to reduce(toggler)  --> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span> <!-- gives to the hamberger (toggler-icon)-->
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About Us</a>
+      </li>
+      <li class="navbar-item">
+      	<a class="nav-link" href="#"> Contact Us </a>
+        
+      </li>
+	
+ 
+
+ </ul>
+    <ul class ="navbar-nav navbar-right ">
+    	<li class="nav-item">
+    		<a class="nav-link" href="login.html">Login &#128130</a>
+    	</li> 
+    	<li class="nav-item">
+    		<a class="nav-link" href ="#">SignIn &#128394</a>
+    	</li>
+    	
+    </ul>  
+
+
+</div>
+</div>
+</nav>
